7d4de8d20da532b31a274946c2d796b6ca1d1caf
Omar ♔ <o_yousaf@live.co.uk>
2024-10-15 14:51:30 +0100

    squashing bugs

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -52,9 +52,19 @@ const Hero = () => {
               </group>
 
               <ambientLight intensity={1} />
-              <directionalLight position={[10, 10, 10]} />
+              <directionalLight position={[10, 10, 10]} intensity={0.5} />
             </Suspense>
           </Canvas>
+        </div>
+
+        <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+          <a href="#about" className="w-fit">
+            <Button
+              name="Let's innovate together"
+              isBeam
+              containerClass="sm:w-fit w-full sm:min-w-96"
+            />
+          </a>
         </div>
       </div>
     </section>
