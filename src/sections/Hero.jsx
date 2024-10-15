df0d32a1452b93080db8497974ca6dfad20fd97b
Omar ♔ <o_yousaf@live.co.uk>
2024-10-15 14:43:39 +0100

    squashing bugs

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
         </p>
 
         <div className="w-full h-full absolute inset-0">
-          {/*<Leva />*/}
+          <Leva hidden />
           <Canvas className="w-full h-full">
             <Suspense fallback={<CanvasLoader />}>
               <PerspectiveCamera makeDefault position={[0, 0, 30]} />
