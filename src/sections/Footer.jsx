b6ad35500fb1c5027411342968432a0efbd88b4d
Omar 🍉 <o_yousaf@live.co.uk>
2025-06-11 14:59:54 +0100

    optimising footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -27,9 +27,7 @@ const Footer = () => {
 
       {/* Copyright */}
       <p className="text-sm text-gray200 tracking-wide">
-        © {new Date().getFullYear()} Built with{" "}
-        <span className="inline-block animate-spin-slow origin-center">💚</span>{" "}
-        by Omar 🍉
+        © {new Date().getFullYear()} Built with Omar 🍉
       </p>
     </footer>
   );
