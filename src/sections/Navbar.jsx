eed31361a132a366061a457f9230b78a1dcb216f
Omar 🍉 <o_yousaf@live.co.uk>
2025-07-24 14:38:16 +0100

    optimising Navbar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -38,8 +38,13 @@ const Navbar = () => {
   const toggleMenu = () => setIsOpen((prev) => !prev);
 
   useEffect(() => {
-    document.body.style.overflow = isOpen ? "hidden" : "auto";
-    document.body.style.overflowX = "hidden"; // Always prevent horizontal scroll
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+      document.documentElement.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+      document.documentElement.style.overflow = "auto";
+    }
   }, [isOpen]);
 
   return (
